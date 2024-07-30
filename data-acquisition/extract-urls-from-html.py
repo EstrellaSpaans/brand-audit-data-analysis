321ac0d315ac1901a93ca40484aa63fe942a09f3
Estrella Spaans-Buckley <64385174+EstrellaSpaans@users.noreply.github.com>
2024-07-30 10:52:43 -0400

    Add extract-urls-from-html.py
    Adding script to extract URLs from beautiful Soup (HTML) object.

---
+++ data-acquisition/extract-urls-from-html.py
@@ -0,0 +1,44 @@
+def extract_urls_from_html(html_content, website):
+    """Extracts product URLs from the given HTML page with Shopify JSON-LD data or fallback to anchor tags."""
+    
+    urls = []
+
+    # Find the script tag containing JSON-LD data
+    script_tag = html_content.find('script', type='application/ld+json')
+    
+    if script_tag and script_tag.string:
+        # Extract the JSON-LD data from the script tag
+        json_data = json.loads(script_tag.string)
+
+        if 'mainEntity' in json_data:
+            main_entity = json_data['mainEntity']
+            
+            # Determine URL extraction logic based on website
+            if website == 'rarebeauty.com':
+                for item in main_entity["itemListElement"]:
+                    extracted_url = item["item"]["url"]
+                    url = '/products' + str(extracted_url).split('/products')[1]
+                    urls.append(url)
+            elif website == 'rembeauty.com':
+                for item in main_entity["itemListElement"]:
+                    extracted_url = item["url"]
+                    url = '/products' + str(extracted_url).split('/products')[1]
+                    urls.append(url)
+            elif website == 'fentybeauty.com':
+                for item in main_entity:
+                    extracted_url = item['offers']['url']
+                    url = '/products' + str(extracted_url).split('/products')[1]
+                    urls.append(url)
+            return urls
+
+    # If JSON-LD extraction fails or is not applicable, use alternative method
+    exclusions = ['/collections', '/product', 'hair', 'skin', 'fragrance']
+    for a in html_content.find_all('a', href=True):
+        href = a['href']
+        if href.startswith('/') and not any(exclusion in href for exclusion in exclusions):
+            urls.append(href)
+
+    # Remove duplicate URLs
+    urls = list(set(urls))
+    
+    return urls
