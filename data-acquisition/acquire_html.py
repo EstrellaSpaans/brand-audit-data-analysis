c4fee51f3f5a8c37122fa641481e419700bb11b0
Estrella Spaans-Buckley <64385174+EstrellaSpaans@users.noreply.github.com>
2024-07-30 10:53:52 -0400

    Upload HTML Acquisition Script

---
+++ data-acquisition/acquire_html.py
@@ -0,0 +1,27 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[ ]:
+
+
+def acquire_html(website_url):
+    """
+    Function to retrieve the HTML from website and turn it into a BeautifulSoup object"""   
+    
+    url = 'https://' + website_url
+    
+    #headers = {'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_10_1) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/39.0.2171.95 Safari/537.36'}
+    headers = {
+        'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/91.0.4472.124 Safari/537.36',
+        'Accept-Language': 'en-US,en;q=0.9',
+        'Accept-Encoding': 'gzip, deflate, br',
+        'Connection': 'keep-alive',
+        'Upgrade-Insecure-Requests': '1',
+        'DNT': '1',  # Do Not Track Request Header
+        'Accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,*/*;q=0.8',
+    }
+    r = requests.get(url,headers=headers, timeout=10)
+    soup = BeautifulSoup(r.content, 'html.parser')
+    
+    return soup
+
